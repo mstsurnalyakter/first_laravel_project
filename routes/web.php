0f370fa1534bc8cc5757690847b09a87ecb29312
mstsurnalyakter <surnalyms@gmail.com>
2024-12-16 08:08:25 -0700

    add routes and views for hello, about, and name pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('', function () {
     return "Hello World";
 });
+
+
+Route::get('/hello', function () {
+    return "<h1>Hello World</h1>";
+});
+
+Route::get('about', function () {
+    return view('pages.about');
+});
+
+Route::get('name',function(){
+    return view('pages.name');
+});
+
+Route::get('/users/{id}/{name}',function($id,$name){
+    return 'This is user '.$name. ' with an id of '.$id;
+});
+
+
